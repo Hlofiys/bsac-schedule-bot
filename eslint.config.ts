66f540a96dc0909f02b90bb01c664d41d8ede474
hlofiys <hlofiys@gmail.com>
2025-09-04 21:50:50 +0300

    build after lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,6 +8,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
+    ignores: ["dist/**", "node_modules/**", ".env", ".env.*"],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
